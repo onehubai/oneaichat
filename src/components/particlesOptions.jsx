58dfd188623718649f0290c2d74a3a86c77ab8f9
onedevai <partamna03@gmail.com>
2024-12-01 22:25:12 +0600

    Create particlesOptions.jsx

---
+++ src/components/particlesOptions.jsx
@@ -0,0 +1,26 @@
+export const particlesOptions = {
+  background: {
+    color: {
+      value: "#f1f1f1",
+    },
+  },
+  particles: {
+    number: {
+      value: 50,
+    },
+    size: {
+      value: 3,
+    },
+    move: {
+      enable: true,
+      speed: 1,
+      direction: "none",
+      outModes: {
+        default: "out",
+      },
+    },
+    opacity: {
+      value: 0.5,
+    },
+  },
+};
